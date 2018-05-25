166b599f2c98504b74071f71ef318bf7a9fe2de5
swethaaraman <39500276+swethaaraman@users.noreply.github.com>
2018-05-25 14:42:26 -0400

    Add files via upload

---
+++ wordcount.py
@@ -0,0 +1,13 @@
+#file = open("f", "r")
+file = open("/Users/swethaa/Desktop/Home/untitled.txt", "r")
+splitfile = file.read()
+splitfile = splitfile.lower()
+convertlist = splitfile.split()
+di = {}
+for eachWord in convertlist:
+    di[eachWord] = di.get(eachWord, 0) + 1
+converttolist = []
+for key, values in di.items():
+    converttolist.append((values, key))
+converttolist.sort()
+print(converttolist)

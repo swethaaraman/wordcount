a17735e12a6eb4f7bcaf3cc89b5dd567af885268
swethaaraman <39500276+swethaaraman@users.noreply.github.com>
2018-05-31 15:56:05 -0400

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,6 @@
-file = open("/Users/swethaa/Desktop/Home/untitled.txt", "r")
-splitfile = file.read()
+import sys
+with open(sys.argv[1], 'r') as file:
+    splitfile = file.read()
 splitfile = splitfile.lower()
 convertlist = splitfile.split()
 di = {}
